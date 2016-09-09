5da6b355744a6d3ceb98823b68ed8269ec0fbbb7
Karl Lundfall <karl@bizboard.nl>
2016-09-09 11:32:46 +0200

    Converted to jspm project in order for the dependency to be read properly

---
+++ jspm.config.js
@@ -0,0 +1,97 @@
+SystemJS.config({
+  browserConfig: {
+    "paths": {
+      "npm:": "/jspm_packages/npm/",
+      "github:": "/jspm_packages/github/",
+      "famous-flex/": "/src/"
+    }
+  },
+  nodeConfig: {
+    "paths": {
+      "npm:": "jspm_packages/npm/",
+      "github:": "jspm_packages/github/",
+      "famous-flex/": "src/"
+    }
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.14"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "famous-flex": {
+      "main": "famous-flex.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  map: {
+    "es6-map": "npm:es6-map@0.1.4",
+    "events": "github:jspm/nodelibs-events@0.2.0-alpha",
+    "path": "github:jspm/nodelibs-path@0.2.0-alpha",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha"
+  },
+  packages: {
+    "npm:es6-map@0.1.4": {
+      "map": {
+        "d": "npm:d@0.1.1",
+        "es5-ext": "npm:es5-ext@0.10.12",
+        "es6-set": "npm:es6-set@0.1.4",
+        "es6-symbol": "npm:es6-symbol@3.1.0",
+        "event-emitter": "npm:event-emitter@0.3.4",
+        "es6-iterator": "npm:es6-iterator@2.0.0"
+      }
+    },
+    "npm:d@0.1.1": {
+      "map": {
+        "es5-ext": "npm:es5-ext@0.10.12"
+      }
+    },
+    "npm:es5-ext@0.10.12": {
+      "map": {
+        "es6-iterator": "npm:es6-iterator@2.0.0",
+        "es6-symbol": "npm:es6-symbol@3.1.0"
+      }
+    },
+    "npm:es6-set@0.1.4": {
+      "map": {
+        "d": "npm:d@0.1.1",
+        "es5-ext": "npm:es5-ext@0.10.12",
+        "es6-iterator": "npm:es6-iterator@2.0.0",
+        "es6-symbol": "npm:es6-symbol@3.1.0",
+        "event-emitter": "npm:event-emitter@0.3.4"
+      }
+    },
+    "npm:es6-symbol@3.1.0": {
+      "map": {
+        "d": "npm:d@0.1.1",
+        "es5-ext": "npm:es5-ext@0.10.12"
+      }
+    },
+    "npm:event-emitter@0.3.4": {
+      "map": {
+        "es5-ext": "npm:es5-ext@0.10.12",
+        "d": "npm:d@0.1.1"
+      }
+    },
+    "npm:es6-iterator@2.0.0": {
+      "map": {
+        "d": "npm:d@0.1.1",
+        "es5-ext": "npm:es5-ext@0.10.12",
+        "es6-symbol": "npm:es6-symbol@3.1.0"
+      }
+    }
+  }
+});
